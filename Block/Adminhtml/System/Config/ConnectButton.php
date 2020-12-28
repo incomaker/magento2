1b06294bbe01230c9f7a29a9bc20356d8f28e3b9
Tomáš Šalamon <salamon@incomaker.com>
2020-12-28 01:49:48 +0100

    Button text

---
+++ Block/Adminhtml/System/Config/ConnectButton.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Incomaker\Magento2\Block\Adminhtml\System\Config;
+
+use Magento\Config\Block\System\Config\Form\Field;
+
+class ConnectButton extends Field
+{
+
+    protected $_template = 'Incomaker_Magento2::system/config/connect_button.phtml';
+
+}
