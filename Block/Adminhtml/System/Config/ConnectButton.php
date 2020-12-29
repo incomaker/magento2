7a317adcaec0053217ed4bd62a225259e5da94b2
Tomáš Šalamon <salamon@incomaker.com>
2020-12-30 00:28:34 +0100

    Button text

--- Block/Adminhtml/System/Config/ConnectButton.php
+++ Block/Adminhtml/System/Config/ConnectButton.php
@@ -1,12 +1,79 @@
 <?php
 
-namespace Incomaker\Magento2\Block\Adminhtml\System\Config;
+namespace MageWorx\AlsoBought\Block\System\Config;
 
+use Magento\Backend\Block\Template\Context;
 use Magento\Config\Block\System\Config\Form\Field;
+use Magento\Framework\Data\Form\Element\AbstractElement;
 
-class ConnectButton extends Field
+class Collect extends Field
 {
-
+    /**
+     * @var string
+     */
     protected $_template = 'Incomaker_Magento2::system/config/connect_button.phtml';
 
+    /**
+     * @param Context $context
+     * @param array $data
+     */
+    public function __construct(
+        Context $context,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * Remove scope label
+     *
+     * @param  AbstractElement $element
+     * @return string
+     */
+    public function render(AbstractElement $element)
+    {
+        $element->unsScope()->unsCanUseWebsiteValue()->unsCanUseDefaultValue();
+        return parent::render($element);
+    }
+
+    /**
+     * Return element html
+     *
+     * @param  AbstractElement $element
+     * @return string
+     */
+    protected function _getElementHtml(AbstractElement $element)
+    {
+        return $this->_toHtml();
+    }
+
+    /**
+     * Return ajax url for collect button
+     *
+     * @return string
+     */
+    public function getAjaxUrl()
+    {
+        return $this->getUrl('incomaker_magento2/system_config/connect');
+    }
+
+    /**
+     * Generate collect button html
+     *
+     * @return string
+     */
+    public function getButtonHtml()
+    {
+        $button = $this->getLayout()->createBlock(
+            'Magento\Backend\Block\Widget\Button'
+        )->setData(
+            [
+                'id' => 'connect_button',
+                'label' => __('Connect'),
+            ]
+        );
+
+        return $button->toHtml();
+    }
 }
+?>
