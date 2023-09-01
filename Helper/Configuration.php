64ba8587a42062b40f7b5f7ecb6fcae220ced87e
Karel Zavadil <zavadil@incomaker.com>
2023-09-01 12:07:21 +0200

    started new conf

--- Helper/Configuration.php
+++ Helper/Configuration.php
@@ -2,17 +2,34 @@
 
 namespace Incomaker\Magento2\Helper;
 
-class Configuration extends \Magento\Framework\App\Helper\AbstractHelper {
+class Configuration
+{
 
-    public function getConfig($config_path, $default = NULL)
-    {
-        $val = $this->scopeConfig->getValue(
-            $config_path, \Magento\Store\Model\ScopeInterface::SCOPE_WEBSITE
-        );
-        if (isset($val)) {
-            return $val;
-        } else {
-            return $default;
-        }
-    }
+	private $scopeConfig;
+	private $configWriter;
+
+	const CONFIG_SCOPE = \Magento\Store\Model\ScopeInterface::SCOPE_WEBSITE;
+
+	public function __construct(
+		\Magento\Framework\App\Config\Storage\WriterInterface $configWriter,
+		\Magento\Store\Model\StoreManagerInterface            $storeManager
+	)
+	{
+		$this->_logger = $logger;
+		$this->_configWriter = $configWriter;
+		$this->_storeManager = $storeManager;
+	}
+
+	public function getConfig($config_path, $default = NULL)
+	{
+		$val = $this->scopeConfig->getValue($config_path, self::CONFIG_SCOPE);
+		return $val ?? $default;
+	}
+
+	public function setConfig($config_path, $value)
+	{
+		$this->scopeConfig->setValue(
+			$config_path, \Magento\Store\Model\ScopeInterface::SCOPE_WEBSITE
+		);
+	}
 }
--- Helper/IncomakerDriver.php
+++ Helper/IncomakerDriver.php
@@ -1,27 +1,34 @@
 <?php
+
 namespace Incomaker\Magento2\Helper;
 
 use Incomaker\Api\Connector;
 
 class IncomakerDriver implements \Incomaker\Api\DriverInterface
 {
-    private $value;
+	const CONFIG_KEY_MAP = [
+		Connector::INCOMAKER_API_KEY => 'incomaker/settings/api_key',
+		Connector::INCOMAKER_ACCOUNT_ID => 'incomaker/settings/account_id',
+		Connector::INCOMAKER_PLUGIN_ID => 'incomaker/settings/plugin_id'
+	];
 
-    public function __construct(
-        Configuration $configuration
-    )
-    {
-        $this->value[Connector::INCOMAKER_API_KEY] = $configuration->getConfig('incomaker/settings/api_key');
-        $this->value[Connector::INCOMAKER_ACCOUNT_ID] = $configuration->getConfig('incomaker/settings/account_id');
-        $this->value[Connector::INCOMAKER_PLUGIN_ID] = $configuration->getConfig('incomaker/settings/plugin_id');
-    }
+	/**
+	 * @var Configuration
+	 */
+	private $configuration;
 
-    public function getSetting($key) {
-        return $this->value[$key];
-    }
+	public function __construct(Configuration $configuration)
+	{
+		$this->configuration = $configuration;
+	}
 
-    public function updateSetting($key, $value) {
-        $this->value[$key] = $value;
-    }
+	public function getSetting($key)
+	{
+		return $this->configuration->getConfig(self::CONFIG_KEY_MAP[$key]);
+	}
 
+	public function updateSetting($key, $value)
+	{
+		$this->configuration->setConfig(self::CONFIG_KEY_MAP[$key], $value);
+	}
 }
